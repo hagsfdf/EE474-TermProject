56d13f179f19f6502b39aeca512bfe211457a6a9
김제민 <39248839+kjm981995@users.noreply.github.com>
2019-05-23 23:48:56 +0900

    Update train.py

--- model/train.py
+++ model/train.py
@@ -1,5 +1,3 @@
-
-
 import os
 import tensorflow as tf
 import numpy as np
@@ -8,77 +6,114 @@ from PIL import Image
 import cv2
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-
-def cam(img_path, label):
-    tf.reset_default_graph()
+from net import CNN_model
 
 
-    img = mpimg.imread(img_path)
-    plt.imshow(img)
-    # plt.show()A
+dir_data = "./image/"
+
+data = np.load(dir_data + "train_data.npy")
+filenames = data['dir']
+labels = data['price']
+N = 100  # len(labels)
+n_class = 2
+
+def label_loading():
+    one_hot = np.zeros([N, n_class], dtype=np.float32)
+
+    for i in range(N):
+        label = labels[i]
+
+        if label < 60000:
+            one_hot[i, :] = [1, 0]
+        elif label >= 60000:
+            one_hot[i, :] = [0, 1]
 
 
-    if os.path.exists(img_path):
-        if img_path[14:] == '.png':
-            img = Image.open(img_path)
-            img = np.array(img, dtype='f')[:, :, :-1]
+    print(one_hot)
+    return one_hot
 
+
+def img_loading():
+    for i in range(N):
+
+        filename = filenames[i]
+
+        if not os.path.exists(filename):
+            filename = filename[:12] + '.png'
+            if os.path.exists(filename):
+                img = plt.imread("./" + filename)
+                img = np.array(img, dtype='f')[:, :, :-1]
         else:
-            img = Image.open(img_path)
+            img = plt.imread("./" + filename)
             img = np.array(img, dtype='f')
-    img = np.reshape(img, [1, 180, 180, 3])
 
-    with tf.Session() as sess:
-        saver = tf.train.import_meta_graph('./model/model.ckpt-200.meta')
-        saver.restore(sess, tf.train.latest_checkpoint('./model/'))
+        img = np.reshape(img, [1, 180, 180, 3])
 
-        input = sess.graph.get_tensor_by_name("x:0")
-        last_conv_layer = sess.graph.get_tensor_by_name('Conv_4/Relu:0')
-
-        grad_cam = sess.run([last_conv_layer], feed_dict={input: img})
-
-        print(np.shape(grad_cam))
+        if i == 0:
+            img_dataset = img
+        else:
+            img_dataset = np.vstack([img_dataset, img])
 
 
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
-    preds = model.predict(x)
+    return img_dataset
 
-    argmax = np.argmax(preds[0])  # Q1. argmax of model prediction result = 284
-    output = model.output[:, argmax]
 
-    last_conv_layer = model.get_layer(
-        'block5_conv3')  # Q2. what is block number & conv. layer number for last conv. layer?
-    grads = K.gradients(output, last_conv_layer.output)[0]
-    pooled_grads = K.mean(grads, axis=(0, 1, 2)) # global average pooling
-    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
-    pooled_grads_value, conv_layer_output_value = iterate([x])
-    for i in range(50):  # Q3. How many iterations?
-        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
-    heatmap = np.mean(conv_layer_output_value, axis=-1)
-    heatmap = np.maximum(heatmap, 0)
-    heatmap /= np.max(heatmap)
-
-    img = cv2.imread(img_path)
-    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-    heatmap = np.uint8(255 * heatmap)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-    hif = .4
-    superimposed_img = heatmap * hif + img* 0.6  # Q4. superimpose heatmap and image
-    output = 'output.jpeg'
-    cv2.imwrite(output, superimposed_img)
-    img = mpimg.imread(output)
-    plt.imshow(img)
-    plt.show()
-
-    return None
 
 
 def main():
+    minibatch_size = 20
+    lear_rate = 0.01
+    bta1 = 0.9
+    bta2 = 0.999
+    epsln = 0.00000001
 
-    cam('./image/000003.png', [1, 0, 0, 0])
+    input = tf.placeholder(tf.float32, shape=[None, 180, 180, 3], name='x')
+    model = CNN_model()
+
+    predicted = model.build(input, n_class, 180, 3, 0.5)
+
+    gt = tf.placeholder(tf.float32, shape=[None, n_class], name='y')
+
+    with tf.name_scope('Loss'):
+        label_loss = tf.reduce_mean(tf.square(predicted - gt))
+        tf.summary.scalar('label_loss', label_loss)
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=lear_rate, beta1=bta1, beta2=bta2, epsilon=epsln).minimize(
+        label_loss)
+
+    saver = tf.train.Saver()
+
+    images = img_loading()
+    labeling = label_loading()
+
+    with tf.Session() as sess:
+
+        test_index = range(N - 3, N)
+        test_data = images[test_index, :, :, :]
+        test_label = labeling[test_index, :]
+
+        train_index = range(0, N)
+        train_index = [x for x in train_index if x not in test_index]
+        train_data = images[train_index, :, :, :]
+        train_label = labeling[train_index, :]
+
+        sess.run(tf.global_variables_initializer())
+        for itr in range(20001):
+            batch_idx = random.sample(range(train_data.shape[0]), minibatch_size)
+            train_img_batch = train_data[batch_idx, :, :, :]
+            train_lbl_batch = train_label[batch_idx, :]
+
+            _ = sess.run([optimizer], feed_dict={input: train_img_batch, gt: train_lbl_batch})
+
+            if itr % 100 == 0:
+                [train_loss] = sess.run([label_loss], feed_dict={input: train_img_batch, gt: train_lbl_batch})
+                [test_loss] = sess.run([label_loss], feed_dict={input: test_data, gt: test_label})
+
+                print('@ iteration : %i, Training loss = %.6f, Test loss = %.6f' % (itr, train_loss, test_loss))
+                saver.save(sess, os.path.join('./model', "model.ckpt"), itr)
+
+        sess.close()
+
 
 if __name__ == '__main__':
     main()
-
-
