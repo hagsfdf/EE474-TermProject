c265e4e43a692534c098d99bdef5945b9b7e901a
김제민 <39248839+kjm981995@users.noreply.github.com>
2019-05-28 21:24:37 +0900

    can Train!! max acc = 70%

--- model/train.py
+++ model/train.py
@@ -14,24 +14,44 @@ dir_data = "./image/"
 data = np.load(dir_data + "train_data.npy")
 filenames = data['dir']
 labels = data['price']
-N = 100  # len(labels)
-n_class = 2
+N = 512  #len(labels) # 1284
+n_class = 4
+
 
 def label_loading():
     one_hot = np.zeros([N, n_class], dtype=np.float32)
 
     for i in range(N):
         label = labels[i]
+        if label < 37050: # 37050 , 52900, 69000
+            one_hot[i, :] = [1, 0, 0, 0]
+        elif label <= 52900:
+            one_hot[i, :] = [0, 1, 0, 0]
+        elif label <= 69000:
+            one_hot[i, :] = [0, 0, 1, 0]
+        else:
+            one_hot[i, :] = [0, 0, 0, 1]
 
-        if label < 60000:
-            one_hot[i, :] = [1, 0]
-        elif label >= 60000:
-            one_hot[i, :] = [0, 1]
-
-
-    print(one_hot)
+    print(sum(one_hot[:,0]), sum(one_hot[:,1]), sum(one_hot[:,2]), sum(one_hot[:,3]))
     return one_hot
 
+def argmax2onehot(trained):
+    argmax = np.argmax(trained, axis=1)
+    n = np.shape(argmax)[0]
+
+    one_hot = np.zeros([n, n_class], dtype=np.float32)
+
+    for i in range(n):
+        if argmax[i] == 0:
+            one_hot[i, :] = [1, 0, 0, 0]
+        elif argmax[i] == 1:
+            one_hot[i, :] = [0, 1, 0 ,0]
+        elif argmax[i] == 2:
+            one_hot[i, :] = [0, 0, 1, 0]
+        elif argmax[i] == 3:
+            one_hot[i, :] = [0, 0, 0, 1]
+
+    return one_hot
 
 def img_loading():
     for i in range(N):
@@ -61,21 +81,22 @@ def img_loading():
 
 
 def main():
-    minibatch_size = 20
-    lear_rate = 0.01
+    minibatch_size = 128
+    lear_rate = 0.000001
     bta1 = 0.9
     bta2 = 0.999
     epsln = 0.00000001
 
     input = tf.placeholder(tf.float32, shape=[None, 180, 180, 3], name='x')
+    is_train = tf.placeholder(tf.bool, name = 'is_train')
     model = CNN_model()
 
-    predicted = model.build(input, n_class, 180, 3, 0.5)
+    predicted = model.build(input, n_class, 180, 3, 1, is_train)
 
     gt = tf.placeholder(tf.float32, shape=[None, n_class], name='y')
 
     with tf.name_scope('Loss'):
-        label_loss = tf.reduce_mean(tf.square(predicted - gt))
+        label_loss = tf.reduce_mean(tf.abs(predicted - gt))
         tf.summary.scalar('label_loss', label_loss)
 
     optimizer = tf.train.AdamOptimizer(learning_rate=lear_rate, beta1=bta1, beta2=bta2, epsilon=epsln).minimize(
@@ -103,14 +124,22 @@ def main():
             train_img_batch = train_data[batch_idx, :, :, :]
             train_lbl_batch = train_label[batch_idx, :]
 
+
             _ = sess.run([optimizer], feed_dict={input: train_img_batch, gt: train_lbl_batch})
 
             if itr % 100 == 0:
-                [train_loss] = sess.run([label_loss], feed_dict={input: train_img_batch, gt: train_lbl_batch})
-                [test_loss] = sess.run([label_loss], feed_dict={input: test_data, gt: test_label})
+                [train_loss] = sess.run([label_loss], feed_dict={input: train_img_batch, gt: train_lbl_batch, is_train: True})
+                [test_loss] = sess.run([label_loss], feed_dict={input: test_data, gt: test_label, is_train: False})
 
                 print('@ iteration : %i, Training loss = %.6f, Test loss = %.6f' % (itr, train_loss, test_loss))
                 saver.save(sess, os.path.join('./model', "model.ckpt"), itr)
+                [trained] = sess.run([predicted], feed_dict={input: train_img_batch, gt: train_lbl_batch, is_train: False})
+
+                acc = (train_lbl_batch + argmax2onehot(trained))
+                acc = np.shape(np.where(acc == 2.))[1]
+                print('acc = %i %%' %(100* acc/ minibatch_size))
+        print('acc')
+
 
         sess.close()
 
