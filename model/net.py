a3d6b13b52a1d2085d0f507ee9eac5fa1777c196
JKpark <satechi22@kaist.ac.kr>
2019-06-01 16:57:15 +0900

    input brand

--- model/net.py
+++ model/net.py
@@ -95,3 +95,58 @@ class CNN_model:
         self.Pred = self.fc2
 
         return self.Pred
+
+class CNN_model_brand:
+
+    def build(self, x, brand, n_classes, nShape, nChannels, dropout, is_training):
+        x = tf.reshape(x, [-1, nShape, nShape, nChannels])
+
+        self.conv11 = tf.layers.conv2d(x, filters=32, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_11')
+        self.conv11 = tf.nn.relu(BN(self.conv11, is_training))
+        self.conv12 = tf.layers.conv2d(self.conv11, filters=32, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_12')
+        self.conv12 = tf.nn.relu(BN(self.conv12, is_training))
+        self.pool1 = pool_layer(self.conv12)
+
+        self.conv21 = tf.layers.conv2d(self.pool1, filters=64, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_21')
+        self.conv21 = tf.nn.relu(BN(self.conv21, is_training))
+        self.conv22 = tf.layers.conv2d(self.conv21, filters=64, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_22')
+        self.conv22 = tf.nn.relu(BN(self.conv22, is_training))
+        self.pool2 = pool_layer(self.conv21)
+
+        self.conv31 = tf.layers.conv2d(self.pool2, filters=128, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_31')
+        self.conv31 = tf.nn.relu(BN(self.conv31, is_training))
+        self.conv32 = tf.layers.conv2d(self.conv31, filters=128, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_32')
+        self.conv32 = tf.nn.relu(BN(self.conv32, is_training))
+        self.pool3 = pool_layer(self.conv31)
+
+
+        self.conv4 = tf.layers.conv2d(self.pool3, filters=256, kernel_size=(5, 5), strides=(1, 1), padding='SAME',
+                                      activation=tf.nn.relu, name='Conv_4')
+
+
+        print(np.shape(self.conv4))
+        self.pool4 = pool_layer(self.conv4)
+        print(self.conv4.name)
+        pool_last = self.pool4
+
+        pool_shape = pool_last.get_shape().as_list()
+
+        flattened = tf.reshape(pool_last, [-1, pool_shape[1] * pool_shape[2] * pool_shape[3]])
+        flattened = tf.concat([flattened, brand],1)
+        flattened_shape = flattened.get_shape().as_list()
+        
+        self.fc1 = fc_layer(flattened, flattened_shape[1], 512, dropoutProb=dropout, name = "FC1")
+        self.fc2 = fc_layer(self.fc1, 512, n_classes, dropoutProb=dropout, name= "FC2")
+
+        print(self.fc2.name)
+        self.fc2 = tf.nn.softmax(self.fc2)
+
+        self.Pred = self.fc2
+
+        return self.Pred
---
+++ model/train_brand.py
@@ -0,0 +1,163 @@
+import os
+import tensorflow as tf
+import numpy as np
+import random
+from PIL import Image
+import cv2
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from net import CNN_model, CNN_model_brand
+
+
+dir_data = "./imagebrand/"
+
+data = np.load(dir_data + "train.npy")
+filenames = data['dir']
+label1 = data['price']
+label2 = data['brand']
+N = 512  #len(label1) # 1284
+n_class = 4
+n_class_brand = 5
+
+def brand_loading():
+    one_hot_brand = np.zeros([N, n_class_brand], dtype = np.float32)
+    for i in range(N):
+        label = label2[i]
+        brand = ['adidas','descente','fila','nike','puma']
+        one_hot_brand[i,:] = np.isin(brand,label)
+    print(np.sum(one_hot_brand, axis=0))
+    return one_hot_brand
+
+def label_loading():
+    one_hot = np.zeros([N, n_class], dtype=np.float32)
+
+    for i in range(N):
+        label = label1[i]
+        if label < 37050: # 37050 , 52900, 69000
+            one_hot[i, :] = [1, 0, 0, 0]
+        elif label <= 52900:
+            one_hot[i, :] = [0, 1, 0, 0]
+        elif label <= 69000:
+            one_hot[i, :] = [0, 0, 1, 0]
+        else:
+            one_hot[i, :] = [0, 0, 0, 1]
+
+    print(np.sum(one_hot,axis = 0))
+    return one_hot
+
+def argmax2onehot(trained):
+    argmax = np.argmax(trained, axis=1)
+    n = np.shape(argmax)[0]
+
+    one_hot = np.zeros([n, n_class], dtype=np.float32)
+
+    for i in range(n):
+        if argmax[i] == 0:
+            one_hot[i, :] = [1, 0, 0, 0]
+        elif argmax[i] == 1:
+            one_hot[i, :] = [0, 1, 0 ,0]
+        elif argmax[i] == 2:
+            one_hot[i, :] = [0, 0, 1, 0]
+        elif argmax[i] == 3:
+            one_hot[i, :] = [0, 0, 0, 1]
+
+    return one_hot
+
+def img_loading():
+    for i in range(N):
+
+        filename = filenames[i]
+
+        if not os.path.exists(filename):
+            filename = filename[:12] + '.png'
+            if os.path.exists(filename):
+                img = plt.imread("./" + filename)
+                img = np.array(img, dtype='f')[:, :, :-1]
+        else:
+            img = plt.imread("./" + filename)
+            img = np.array(img, dtype='f')
+
+        img = np.reshape(img, [1, 180, 180, 3])
+
+        if i == 0:
+            img_dataset = img
+        else:
+            img_dataset = np.vstack([img_dataset, img])
+
+
+    return img_dataset
+
+
+
+
+def main():
+    minibatch_size = 128
+    lear_rate = 0.000001
+    bta1 = 0.9
+    bta2 = 0.999
+    epsln = 0.00000001
+
+    input = tf.placeholder(tf.float32, shape=[None, 180, 180, 3], name='x')
+
+    ## 차원 알려주세욤
+    input_brand = tf.placeholder(tf.float32, shape = [], name = 'brand')
+    ## need shape
+    is_train = tf.placeholder(tf.bool, name = 'is_train')
+    model = CNN_model_brand()
+
+    predicted = model.build(input,brand, n_class, 180, 3, 1, is_train)
+
+    gt = tf.placeholder(tf.float32, shape=[None, n_class], name='y')
+
+    with tf.name_scope('Loss'):
+        label_loss = tf.reduce_mean(tf.abs(predicted - gt))
+        tf.summary.scalar('label_loss', label_loss)
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=lear_rate, beta1=bta1, beta2=bta2, epsilon=epsln).minimize(
+        label_loss)
+
+    saver = tf.train.Saver()
+
+    images = img_loading()
+    labeling = label_loading()
+    brand = brand_loading()
+    
+    with tf.Session() as sess:
+
+        test_index = range(N - 3, N)
+        test_data = images[test_index, :, :, :]
+        test_label = labeling[test_index, :]
+
+        train_index = range(0, N)
+        train_index = [x for x in train_index if x not in test_index]
+        train_data = images[train_index, :, :, :]
+        train_label = labeling[train_index, :]
+
+        sess.run(tf.global_variables_initializer())
+        for itr in range(20001):
+            batch_idx = random.sample(range(train_data.shape[0]), minibatch_size)
+            train_img_batch = train_data[batch_idx, :, :, :]
+            train_lbl_batch = train_label[batch_idx, :]
+
+
+            _ = sess.run([optimizer], feed_dict={input: train_img_batch, gt: train_lbl_batch})
+
+            if itr % 100 == 0:
+                [train_loss] = sess.run([label_loss], feed_dict={input: train_img_batch, gt: train_lbl_batch, is_train: True})
+                [test_loss] = sess.run([label_loss], feed_dict={input: test_data, gt: test_label, is_train: False})
+
+                print('@ iteration : %i, Training loss = %.6f, Test loss = %.6f' % (itr, train_loss, test_loss))
+                saver.save(sess, os.path.join('./model', "model.ckpt"), itr)
+                [trained] = sess.run([predicted], feed_dict={input: train_img_batch, gt: train_lbl_batch, is_train: False})
+
+                acc = (train_lbl_batch + argmax2onehot(trained))
+                acc = np.shape(np.where(acc == 2.))[1]
+                print('acc = %i %%' %(100* acc/ minibatch_size))
+        print('acc')
+
+
+        sess.close()
+
+
+if __name__ == '__main__':
+    main()
